d8e33ec60b2b4351e4bc302f4464defe1b6c8848
sachour <sachour@mit.edu>
2016-01-15 11:53:38 -0500

    scraper scaffold

--- public/js/scrape.js
+++ public/js/scrape.js
@@ -1,6 +1,7 @@
 var Scraper = function(url,n){
    this.init = function(url,n){
       this.root = url;
+      this.dummy = $("<div/>")
       this.npages = n;
    }
 
@@ -21,13 +22,58 @@ var Scraper = function(url,n){
             else{
                that.data = data
                that.dom = jQuery.parseHTML(that.data.body);
+               proc(that.dom);
             }
          }
       })
    }
+   this.get_works = function(D){
+      var get_work_data = function(w){
+         var data = {};
+         //get title
+         data.title = $("a",$(".heading",w)).html();
+         //get url
+         var url = $("a",$(".heading",w)).attr("href")
+         data.id = url 
+         data.url = "http://www.archiveofourown.com"+url
+         //get author
+         data.author = $("a",$("[rel=author]",w)).html()
+         data.author_url = $("a",$("[rel=author]",w)).attr("href")
+         //get date created 
+         data.date = $(".datetime",w).html()
+         //get fandoms
+         //get warnings
+         //get tags
+         
+         //get summary
+         data.summary = $("p", $(".userstuff.summary",w)).html();
+
+         //get stats
+         var stats = {};
+         var elem = $(".stats",w);
+         stats.language = $("dd.language",elem).html();
+         stats.words = $("dd.words",elem).html();
+         stats.hits = $("dd.hits",elem).html();
+         stats.chapters = $("dd.chapters",elem).html();
+         data.stats = stats;
+
+         return data;
+      }
+      var works = []
+      $(".work",D).each(function(i,e){
+         var d = get_work_data(d);
+         console.log(d);
+         works.push(d);
+      })
+      return works;
+   }
    this.scrape_page = function(url){
+      var that = this;
       var proc_page = function(pg){
-         console.log(pg);
+         var works = that.get_works(pg);
+         //var next_url = this.get_next_url(pg);
+         //var works = this.fill_works(pg);
+
 
       }
       this.get_page(url, proc_page);
